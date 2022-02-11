d701933119f5a1026555056cd6fc123662e02ebb
Dada2201 <dleneveu22@gmail.com>
2022-02-11 14:57:51 +0100

    Call service to populate repo result

--- src/app/repo-result/repo-result.component.ts
+++ src/app/repo-result/repo-result.component.ts
@@ -10,23 +10,24 @@ export class RepoResultComponent implements OnInit {
   apiStatus: any;
   randomrepo: any;
 
-  constructor(private statusService: StatusService) {}
-  
+  constructor(private statusService: StatusService) { }
+
   ngOnInit(): void {
     this.statusService
       .getStatus()
       .subscribe((response: any) => {
         this.apiStatus = response;
-      });
-
-      this.statusService
-      .getRepo()
-      .subscribe((response: any) => {
-        this.randomrepo = response[0];
-      });
+      })
   }
 
   get status() { return (this.apiStatus && this.apiStatus.status) ? this.apiStatus.status : null }
 
-  get repo() { return (this.randomrepo && this.randomrepo.github_url) ? this.randomrepo.github_url : null }
+  randomRepo(){
+    this.statusService
+    .getRepo()
+    .subscribe( 
+      res =>{
+      this.randomrepo = res
+    })
+  }
 }
--- src/server.js
+++ src/server.js
@@ -56,7 +56,7 @@ app.get('/api/randomrepo', function(req, res, next) {
   
     db.connect();
   
-      db.query('SELECT * FROM `repos` WHERE `id` = 1', function (error, results, fields) {
+      db.query("SELECT * FROM repos AS r1 JOIN (SELECT (RAND() * (SELECT MAX(id) FROM repos)) AS id) AS r2 WHERE r1.id >= r2.id ORDER BY r1.id ASC LIMIT 1", function (error, results, fields) {
         // error will be an Error if one occurred during the query
         // results will contain the results of the query
         // fields will contain information about the returned results fields (if any)
@@ -64,4 +64,3 @@ app.get('/api/randomrepo', function(req, res, next) {
         res.status(200).json(results);
         });
 });
-  
