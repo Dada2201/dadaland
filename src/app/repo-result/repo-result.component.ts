44daef18821eb1e723da21a617059a270eb1b3e8
Dada2201 <dleneveu22@gmail.com>
2022-02-13 21:53:46 +0100

    Home is now only home / RandomRepo is now displaying more info

--- src/app/repo-result/repo-result.component.ts
+++ src/app/repo-result/repo-result.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { StatusService } from '../shared/status.service';
 import { first } from 'rxjs/operators'
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
+
   selector: 'app-repo-result',
   templateUrl: './repo-result.component.html',
   styleUrls: ['./repo-result.component.scss']
@@ -11,14 +13,19 @@ export class RepoResultComponent implements OnInit {
   apiStatus: any;
   randomrepo: any;
 
-  constructor(private statusService: StatusService) { }
-
+  constructor(
+    private statusService: StatusService,
+    private sanitazer : DomSanitizer) {
+   }
+  
   ngOnInit(): void {
     this.statusService
       .getStatus()
       .subscribe((response: any) => {
         this.apiStatus = response;
       })
+
+      this.randomRepo()
   }
 
   get status() { return (this.apiStatus && this.apiStatus.status) ? this.apiStatus.status : null }
@@ -31,5 +38,9 @@ export class RepoResultComponent implements OnInit {
         res => {
           this.randomrepo = res
         })
+  }
+
+  getAvatar(url : string) {
+    return this.sanitazer.bypassSecurityTrustResourceUrl(url);
   }
 }
